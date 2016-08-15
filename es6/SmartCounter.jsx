42dfa7afab24df866bdf03491ee2c79772ff7b80
toyama satoshi <toyamarinyon@gmail.com>
2016-08-15 22:49:23 +0900

    Update

--- es6/SmartCounter.jsx
+++ es6/SmartCounter.jsx
@@ -1,37 +1,65 @@
 import React, { PropTypes, Component } from 'react';
 
 const propTypes = {
-  value: PropTypes.number
+  value: PropTypes.number,
+  max: PropTypes.number,
+  min: PropTypes.number,
+  step: PropTypes.number,
+  prefix: PropTypes.string,
+  suffix: PropTypes.string,
+  onChange: PropTypes.func
 };
 
 const defaultProps = {
-  value: 0
+  value: 0,
+  max: 9999,
+  min: -9999,
+  step: 1,
+  prefix: '',
+  suffix: '',
+  onChange: e => e
 };
 
 class SmartCounter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: props.value
-    };
-    this.incrementValue = this.incrementValue.bind(this);
-    this.decrementValue = this.decrementValue.bind(this);
+    this.state = { value: props.value };
+    this.handleOnClickIncrementButton = this.handleOnClickIncrementButton.bind(this);
+    this.handleOnClickDecrementButton = this.handleOnClickDecrementButton.bind(this);
   }
 
-  incrementValue() {
-    this.setState({ value: this.state.value + 1 });
+  incrementable() {
+    return this.state.value < this.props.max;
   }
 
-  decrementValue() {
-    this.setState({ value: this.state.value - 1 });
+  decrementable() {
+    return this.state.value > this.props.min;
+  }
+
+  handleOnClickIncrementButton() {
+    if (this.incrementable()) {
+      const incrementedValue = this.state.value + this.props.step;
+      this.props.onChange(incrementedValue);
+      this.setState({ value: incrementedValue });
+    }
+  }
+
+  handleOnClickDecrementButton() {
+    if (this.decrementable()) {
+      const decrementedValue = this.state.value - this.props.step;
+      this.props.onChange(decrementedValue);
+      this.setState({ value: decrementedValue });
+    }
   }
 
   render() {
     return (
       <div className="SmartCounter">
+        <span className="presentation-prefix">{this.props.prefix}</span>
         <span className="presentation">{this.state.value}</span>
-        <span className="increment" onClick={this.incrementValue}>+</span>
-        <span className="decrement" onClick={this.decrementValue}>-</span>
+        <span className="presentation-suffix">{this.props.suffix}</span>
+        <span className="increment" onClick={this.handleOnClickIncrementButton}>+</span>
+        <span className="decrement" onClick={this.handleOnClickDecrementButton}>-</span>
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import SmartCounter from '../es6/SmartCounter';
 
 render(
-  <SmartCounter />,
+  <SmartCounter max={5} min={0} step={0.5} suffix="以上" onChange={(value) => console.log(value)}/>,
   document.getElementById('root')
 );
 
